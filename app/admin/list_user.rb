73aa9653a71a6532d12d12ae31416f477473baf4
Andy Webber <andy@aligature.com>
2014-08-09 15:48:19 -0400

    Added permit_params

--- app/admin/list_user.rb
+++ app/admin/list_user.rb
@@ -12,18 +12,6 @@ ActiveAdmin.register ListUser do
       actions
    end
 
-  # See permitted parameters documentation:
-  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #  permitted = [:permitted, :attributes]
-  #  permitted << :other if resource.something?
-  #  permitted
-  # end
-
+   permit_params :list_id, :user_id
 
 end
