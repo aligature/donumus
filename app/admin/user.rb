5cd8f750da2af4c9e9af5123ce11f74508a58c96
Andy Webber <andy@aligature.com>
2014-08-09 15:49:25 -0400

    clean user edit view and interface

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,19 +1,31 @@
 ActiveAdmin.register User do
 
 
-  # See permitted parameters documentation:
-  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  permit_params :username, :name, :administrator
-  #
-  # or
-  #
-  # permit_params do
-  #  permitted = [:permitted, :attributes]
-  #  permitted << :other if resource.something?
-  #  permitted
-  # end
+   # See permitted parameters documentation:
+   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+   permit_params :username, :name, :birthday, :administrator
+
+   index do
+      selectable_column
+      column :id
+      column :username
+      column :name
+      column :birthday
+      column :last_sign_in_at
+      column :administrator
+      actions
+   end
+
+   form do |f|
+      f.inputs "User" do
+         f.input :username
+         f.input :name
+         f.input :birthday, :as => :date_picker
+         f.input :administrator
+      end
+      f.actions
+   end
+
 
 
 end
