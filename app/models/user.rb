3133f1cbf5d0323dc5be3848400268165c468c8c
Andy Webber <andy@aligature.com>
2014-09-04 23:51:30 -0400

    added omniauth_providers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :rememberable, :trackable, :omniauthable
+  devise :rememberable, :trackable, :omniauthable, :omniauth_providers => [:google_oauth2]
      #:database_authenticatable, :registerable, :recoverable, :validatable,
 
 
