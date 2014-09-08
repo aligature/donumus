2da426056c7c21cb673440bb6c91bcc9f142ea6d
Andy Webber <andy@aligature.com>
2014-09-07 20:15:57 -0400

    updated summary and added label

--- app/models/status.rb
+++ app/models/status.rb
@@ -3,11 +3,16 @@ class Status < ActiveRecord::Base
    belongs_to :added_by_user, class_name: "User",
                               foreign_key: "added_by_user_id"
 
-   enum status: { available: 0, looking: 10, partially_gone: 20, gone: 30}
+   enum(status: { available: 0, looking: 10, partially_gone: 20, gone: 30})
 
    def self.summary(statuses)
       statuses = statuses.sort_by {|status| self.statuses[status.status]}
-      statuses.empty? ? "" : statuses.last.status
+      statuses.empty? ? "available" : statuses.last.status
+   end
+
+   def self.label(status)
+      labels = { "available" => "", "looking" => "looking", "partially_gone" => "partial", "gone" => "gone"}
+      return labels[status]
    end
 
    def to_s
