00bca248439314c4f37bb2897e8debf1f73d952a
Andy Webber <andy@aligature.com>
2014-10-18 15:04:48 -0400

    Added logged in user to breadcrumb and added helper function for user breadcrumb display.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+   def user_breadcrumb(view_user, current_user, create_link)
+      if view_user != current_user
+         label = view_user.name
+      else
+         label = "me"
+      end
+
+      if create_link
+         return link_to label, user_path(view_user)
+      else
+         return label
+      end
+   end
+
 end
