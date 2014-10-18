d847ba7489b95a8d40e1c18401392b64b74f02e6
Andy Webber <andy@aligature.com>
2014-10-18 15:03:58 -0400

    Fixed whitespace.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,30 +1,30 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+   # Prevent CSRF attacks by raising an exception.
+   # For APIs, you may want to use :null_session instead.
+   protect_from_forgery with: :exception
 
-  def new_session_path(scope)
-     new_user_session_path
-  end
+   def new_session_path(scope)
+      new_user_session_path
+   end
 
-  def authenticate_admin_user!
-     raise SecurityError unless current_user and current_user.administrator?
-     #authenticate_user! 
-     #unless current_user.admin?
-        #flash[:alert] = "This area is restricted to administrators only."
-        #redirect_to root_path 
-     #end
-  end
+   def authenticate_admin_user!
+      raise SecurityError unless current_user and current_user.administrator?
+      #authenticate_user! 
+      #unless current_user.admin?
+      #flash[:alert] = "This area is restricted to administrators only."
+      #redirect_to root_path 
+      #end
+   end
 
-  def current_admin_user
-     return nil if user_signed_in? && !current_user.admin?
-     current_user
-  end
+   def current_admin_user
+      return nil if user_signed_in? && !current_user.admin?
+      current_user
+   end
 
-  def maybe_redirect(ok)
-     if !ok
-        redirect_to root_path
-     end
-  end
+   def maybe_redirect(ok)
+      if !ok
+         redirect_to root_path
+      end
+   end
 
 end
